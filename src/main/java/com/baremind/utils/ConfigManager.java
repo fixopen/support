1b16056680d69d3d0e9831619bcd5690976fdb0b
DuanShengpeng <duanshp@chineseall.com>
2015-08-16 16:25:46 +0800

    add config manager

---
+++ src/main/java/com/baremind/utils/ConfigManager.java
@@ -0,0 +1,17 @@
+package com.baremind.utils;
+
+import com.baremind.algorithm.Config;
+
+/**
+ * Created by fixopen on 4/6/15.
+ */
+public class ConfigManager {
+    public static Config parseConfig(String configFilePath) {
+        Config result = null;
+        return result;
+    }
+
+    public static void saveConfig(Config config, String configFilePath) {
+        //
+    }
+}
