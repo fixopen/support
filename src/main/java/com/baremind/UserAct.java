4cc7f61fa48ff4209fbaad7be3271c19a37768ac
DuanShengpeng <duanshp@chineseall.com>
2015-08-26 11:07:54 +0800

    fix user post bug

--- src/main/java/com/baremind/UserAct.java
+++ src/main/java/com/baremind/UserAct.java
@@ -4,7 +4,6 @@ import com.baremind.data.Account;
 import com.baremind.data.User;
 import com.baremind.utils.IdGenerator;
 import com.baremind.utils.JPAEntry;
-import com.google.gson.Gson;
 
 import javax.persistence.EntityManager;
 import javax.ws.rs.*;
@@ -16,20 +15,16 @@ import javax.ws.rs.core.Response;
  */
 @Path("user")
 public class UserAct {
-
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getById(@CookieParam("sessionId") String sessionId, @PathParam("id") Long id) {
         Response result = Response.status(401).build();
         if (JPAEntry.isLogining(sessionId)) {
-            String sql = "SELECT u FROM User u WHERE u.id=:id";
-            EntityManager em = JPAEntry.getEntityManager();
-            User user = em.createQuery(sql ,User.class).setParameter("id",id).getSingleResult();
-            if(user!= null){
-               result = Response.ok(new Gson().toJson(user),MediaType.APPLICATION_JSON).build();
-            }else{
-               result = Response.status(404).build();
+            result = Response.status(404).build();
+            User user = JPAEntry.getObject(User.class, "id", id);
+            if (user != null) {
+               result = Response.ok(user, MediaType.APPLICATION_JSON).build();
             }
         }
         return result;
@@ -40,27 +35,22 @@ public class UserAct {
     public Response postUser(@CookieParam("sessionId") String sessionId, User user) {
         Response result = Response.status(401).build();
         if (JPAEntry.isLogining(sessionId)) {
-//            {"id":123,"no":"sdf","name":"sdf","idType":123,"idNo":"adsf","organizationId":null,"companyName":null,"companyNo":null,"position":null,"adress":null}
             user.setId(IdGenerator.getNewId());
             EntityManager em = JPAEntry.getEntityManager();
             em.getTransaction().begin();
             em.persist(user);
-//            em.getTransaction().commit();
-            //{"id":23,"subjectType":"ads","subjectId":12312,"active":0,"type":0,}
             Account account = new Account();
             account.setId(IdGenerator.getNewId());
-            account.setSubjectId(10000l);
+            account.setSubjectId(user.getId());
             account.setSubjectType("Personal");
             account.setActive(0);
             account.setType(0);
             account.setLoginName(user.getName());
             account.setPassword(user.getName());
-//            em.getTransaction().begin();
             em.persist(account);
             em.getTransaction().commit();
             result = Response.ok(account).build();
         }
         return result;
     }
-
 }
